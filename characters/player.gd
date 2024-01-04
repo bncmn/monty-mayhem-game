e06a4f6cb05a36ca558227718ab1aa3c868572c7
dbuencam <dbuencam@sfu.ca>
2024-01-03 17:10:26 -0800

    Convert to point-and-click movement. Begin implementing resource nodes.

--- characters/player.gd
+++ characters/player.gd
@@ -1,17 +1,14 @@
 extends CharacterBody2D
 
-@export var moveSpeed : float = 100
+@export var moveSpeed : float = 500
+var targetLocation = global_position
+
+func _input(event):
+	if event.is_action_pressed("mouseLeft"):
+		targetLocation = get_global_mouse_position()
 
 func _physics_process(delta):
-	# Read movement
-	var inputDirection = Vector2(
-		Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft"),
-		Input.get_action_strength("moveDown") - Input.get_action_strength("moveUp")
-	)
-	print(inputDirection) # DEBUG
-	
-	# Update velocity
-	velocity = inputDirection * moveSpeed
-	
-	# Move character
-	move_and_slide()
+	velocity = global_position.direction_to(targetLocation) * moveSpeed
+	if global_position.distance_to(targetLocation) > 10:
+		# move_and_collide needed some sort of vector in the first argument. It is not used anywhere else.
+		move_and_collide(velocity * delta)
