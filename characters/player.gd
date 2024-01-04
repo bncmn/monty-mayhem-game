f11f9cd09f0f994e83bdc3b92e8aeed872e6a107
dbuencam <dbuencam@sfu.ca>
2024-01-04 10:05:13 -0800

    Fix errors from previous merge.

--- characters/player.gd
+++ characters/player.gd
@@ -10,5 +10,5 @@ func _input(event):
 func _physics_process(delta):
 	velocity = global_position.direction_to(targetLocation) * moveSpeed
 	if global_position.distance_to(targetLocation) > 10:
-		# move_and_collide needed some sort of vector in the first argument. It is not used anywhere else.
+		# move_and_collide needed some sort of vector in the first argument.
 		move_and_collide(velocity * delta)
--- characters/resourceNode.gd
+++ characters/resourceNode.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+# Randomize the number of clicks it takes to "harvest" the resource.
 var clicksToDie = RandomNumberGenerator.new().randi_range(2,5)
 var currentClicks = 0;
 
@@ -8,12 +9,15 @@ func _ready():
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
+		currentClicks += 1
+		print("DEBUG: Tree clicked! ", currentClicks, "/", clicksToDie)
 		# TODO: Write logic for resource harvesting here.
 		if (currentClicks != clicksToDie):
+			# Every click adds a resource material to the player inventory.
 			PlayerInventory.add_item("wood")
-			print(PlayerInventory.get_inventory())
-			currentClicks += 1
-		print("DEBUG: Tree clicked! ", currentClicks, "/", clicksToDie)
 		if (currentClicks == clicksToDie):
+			PlayerInventory.add_item("wood")
+			# Remove resource node from game map once resource has been fully harvested.
 			queue_free()
+		print("DEBUG:", PlayerInventory.get_inventory())
 		
