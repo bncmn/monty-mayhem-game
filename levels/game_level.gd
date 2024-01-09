4d92bce29eb2f0b50b5e5914108375b27df41605
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-01-09 03:37:05 -0800

    Collect wood into player inventory.

--- levels/game_level.gd
+++ levels/game_level.gd
@@ -8,3 +8,13 @@ func _on_base_inv_ui_closed():
 func _on_base_inv_ui_opened():
 	get_tree().paused = true
 
+
+func _on_player_inv_area_mouse_entered():
+	get_tree().paused = true
+
+
+func _on_player_inv_area_mouse_exited():
+	if $BaseUI/BaseInvUI.isOpen:
+		get_tree().paused = true
+	else: 
+		get_tree().paused = false
