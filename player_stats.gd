8903582f0ec0046834ab7231fe43eacabaacaf68
Cass <codingcass@gmail.com>
2024-01-11 12:26:53 -0800

    Health bar code
    in progress

--- player_stats.gd
+++ player_stats.gd
@@ -1,1 +1,32 @@
-extends Node
+extends Control
+
+var player_health : float = 100
+var max_health : float = 100
+
+var health_bar : TextureProgressBar
+
+func _ready():
+	# Assuming you have a TextureProgress node named "health_bar" in your scene
+	health_bar = $TextureProgress
+
+	# Set initial health and max value of the health bar
+	player_health = max_health
+	health_bar.max_value = max_health
+	health_bar.value = player_health
+
+func decrease_health(amount: float):
+	player_health -= amount
+	if player_health < 0:
+		player_health = 0
+
+	update_health_bar()
+
+func increase_health(amount: float):
+	player_health += amount
+	if player_health > max_health:
+		player_health = max_health
+
+	update_health_bar()
+
+func update_health_bar():
+	health_bar.value = player_health
