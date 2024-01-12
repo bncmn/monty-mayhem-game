730bb039ac24c1828711c9d72accd50cb2a4734f
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-01-12 05:46:16 -0800

    Update crafting.

--- ui/menu/craft/CraftSlot1.gd
+++ ui/menu/craft/CraftSlot1.gd
@@ -3,6 +3,7 @@ extends Panel
 @onready var axeText = $Item1
 
 var axe = "res://assets/placeholders/wood axe_placeholder.png"
+var axeBroken = "res://assets/icon.svg"
 var axePress = false
 
 func _on_axe_pressed():
@@ -20,7 +21,18 @@ func _on_craft_button_pressed():
 		BaseInventory.resources.has("iron") && BaseInventory.resources["iron"] >= 1:
 			BaseInventory.removeResource("wood")
 			BaseInventory.removeResource("iron")
+			PlayerInventory.addTool("axe", 10)
 			
 			axeText.texture = load(axe)
 			axeText.expand_mode = 1
 			axeText.size = Vector2(80, 80)
+			
+	PlayerInventory.printToConsole()
+	BaseInventory.printToConsole()
+
+func _process(delta):
+	if PlayerInventory.tools.has("axe") && PlayerInventory.tools["pickaxe"]["durabilityRemaining"] == 0:
+		axeText.texture = load(axeBroken)
+		axeText.expand_mode = 1
+		axeText.size = Vector2(80, 80)
+		PlayerInventory.printToConsole()
