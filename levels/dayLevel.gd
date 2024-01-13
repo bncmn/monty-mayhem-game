8acdf7635032b0902d7404e1c8bb42298c34a3ce
dbuencam <dbuencam@sfu.ca>
2024-01-12 17:35:52 -0800

    Implement day-night cycling including enemy spawn and despawn.

--- levels/dayLevel.gd
+++ levels/dayLevel.gd
@@ -9,9 +9,10 @@ extends Node2D
 
 var randX
 var randY
+var enemies = []
 
 func _ready():
-	#generateResources(1200)
+	generateResources(1200)
 	$dayPhaseTimer.start()
 
 func _on_base_inv_ui_closed():
@@ -52,13 +53,14 @@ func generateEnemies(amountOfEnemies):
 		var enemy = preload("res://interactables/enemy.tscn").instantiate()
 		
 		# Get a random location within the ranges provided.
-		randX = randi_range(-1500, 2500)
-		randY = randi_range(-500, 1500)
+		randX = randi_range(-2500, 3500)
+		randY = randi_range(-850, 1750)
 		
 		# If the random location is NOT within a distance of the previously spawned node, spawn it...
 		if abs(randX - prevX) > maxDistance and abs(randY - prevY) > maxDistance:
 			enemy.global_position = Vector2(randX, randY)
 			add_child(enemy)
+			enemies.append(enemy)
 			prevX = randX
 			prevY = randY
 		# ...otherwise, add one to reroll.
@@ -94,4 +96,12 @@ func generateResources(amountOfResourceNodes):
 			amountOfResourceNodes += 1
 
 func _on_day_phase_timer_timeout():
-	generateEnemies(10)
+	generateEnemies(30)
+	$nightPhaseTimer.start()
+
+func _on_night_phase_timer_timeout():
+	for enemy in enemies:
+		remove_child(enemy)
+		enemy.queue_free()
+	enemies.clear()
+	$dayPhaseTimer.start()
