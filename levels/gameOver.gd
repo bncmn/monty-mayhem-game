b7ee8ea7090f914ae0b3242a8feeb23356732e06
Steven Nguyen <71040947+Partyman901@users.noreply.github.com>
2024-01-13 16:55:21 -0800

    Add Gameover functionality + Gameover buttons

--- levels/gameOver.gd
+++ levels/gameOver.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
+
+func _on_no_exit_button_pressed():
+	get_tree().change_scene_to_file("res://levels/StartMenu.tscn")
