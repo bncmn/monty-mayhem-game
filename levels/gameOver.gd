59a25410400dbc0ff87240aaa33b3c8b272ffe16
Maister8 <Maib.nguyen@outlook.com>
2024-01-14 03:41:03 -0800

    start/gameover menus updated

--- levels/gameOver.gd
+++ levels/gameOver.gd
@@ -1,11 +1,12 @@
 extends Control
 
 #@onready var gameOverTheme = $gameOverTheme
-
+@onready var buttonClick = $buttonClick
 # func _on_ready():
 	# gameOverTheme.play()
 
 func _on_restart_button_pressed():
+	
 	get_tree().reload_current_scene()
 
 func _on_no_exit_button_pressed():
