1fb86401f24596990bf72afa44bbb3131ab0f095
Steven Nguyen <71040947+Partyman901@users.noreply.github.com>
2024-01-14 03:34:34 -0800

    Add button sounds

--- levels/gameOver.gd
+++ levels/gameOver.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var buttonClick = $"../../ButtonContinue"
 #@onready var buttonClicked = $buttonClicked
 # @onready var gameOverTheme = $gameOverTheme
 
@@ -7,7 +8,11 @@ extends Control
 	# gameOverTheme.play()
 
 func _on_restart_button_pressed():
+	buttonClick.play()
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_no_exit_button_pressed():
+	get_tree().paused = false
+	buttonClick.play()
 	get_tree().change_scene_to_file("res://levels/StartMenu.tscn")
